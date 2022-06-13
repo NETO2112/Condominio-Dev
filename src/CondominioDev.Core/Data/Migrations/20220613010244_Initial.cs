34a87cd300d9b8b1dcb081d291fe20a9714ac087
Neto <antonio97.tij@gmail.com>
2022-06-12 22:03:52 -0300

    Primeira Migration

---
+++ src/CondominioDev.Core/Data/Migrations/20220613010244_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CondominioDev.Core.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habitantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", unicode: false, nullable: false),
+                    Sobrenome = table.Column<string>(type: "varchar(100)", unicode: false, nullable: false),
+                    CPF = table.Column<string>(type: "varchar(14)", unicode: false, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "date", nullable: false),
+                    Renda = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habitantes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Habitantes");
+        }
+    }
+}
